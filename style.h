242128885d31baf7dcca34758037d0ce097afdf5
Harsh Uttamchandani <harshuttamchandani2002@gmail.com>
2022-01-17 00:59:03 +0530

    Add files via upload

---
+++ style.h
@@ -0,0 +1,17 @@
+#define maxx getmaxx()
+#define maxy getmaxy()
+#define midx maxx/2
+#define midy maxy/2
+#define bkcolor setbkcolor(4)
+#define fillcol1 setcolor(col1)
+#define fillcol2 setcolor(col2)
+#define fillcol3 setcolor(col3)
+#define left   midx+20
+#define top    midx- 115
+#define right  midx - 45
+#define bottom  midx - 95
+
+#define court_style setfillstyle(1,2)
+#define racetrack_style setfillstyle(1,8)
+#define divider_show setfillstyle(1,7)
+#define loop1  r=0;r<455;r+=30
